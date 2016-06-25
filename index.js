2039211332e44c248e375e87b77d99aca8f4e98a
along <bpd729@163.com>
2016-06-25 18:03:44 +0800

    add dingSSO dingEnterprise api update suite

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
  * Created by along on 16/6/22.
  */
 
-exports.Suite = require('./lib/suite');
-exports.SuiteCallBack = require('./lib/suite_callback');
+exports.Suite = require('./lib/ddingtalk_suite');
+exports.SuiteCallBack = require('./lib/ddingtalk_suite_callback');
+exports.SSO = require('./lib/dingtalk_sso');
+exports.Enterprise = require('./lib/ddingtalk_enterprise');
