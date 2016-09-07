d39087f5066414055ed61f207a977d466705aad6
along <bpd729@163.com>
2016-09-07 18:16:57 +0800

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by along on 16/6/22.
  */
 
-exports.Suite = require('./lib/ddingtalk_suite');
-exports.SuiteCallBack = require('./lib/ddingtalk_suite_callback');
+exports.Suite = require('./lib/dingtalk_suite');
+exports.SuiteCallBack = require('./lib/dingtalk_suite_callback');
 exports.SSO = require('./lib/dingtalk_sso');
-exports.Enterprise = require('./lib/ddingtalk_enterprise');
+exports.Enterprise = require('./lib/dingtalk_enterprise');
